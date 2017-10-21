80fc8d0aeae5d6901d562105d067acd8381e4ddd
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2017-10-21 03:57:13 -0500

    Fix error with select fields.

--- Nibble/NibbleForms/Field/Select.php
+++ Nibble/NibbleForms/Field/Select.php
@@ -7,7 +7,7 @@ class Select extends Options
     {
         $field = sprintf('<select name="%1$s" id="%2$s_%1$s">', $name, $form_name);
         foreach ($this->options as $key => $val) {
-            $attributes = $this->getAttributeString($val);
+            $attributes = $this->_getAttributeString($val);
             $field .= sprintf('<option value="%s" %s>%s</option>', $key, ((string) $key === (string) $value ? 'selected="selected"' : '') . $attributes['string'], $attributes['val']);
         }
         $field .= '</select>';
