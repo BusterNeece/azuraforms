744bee0bf59ce4061c8f400ca7575a09dbc4afc4
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-10-14 14:11:06 -0500

    Fix the array notation for PHP parsing of multiple select values.

--- src/Field/MultipleSelect.php
+++ src/Field/MultipleSelect.php
@@ -6,7 +6,7 @@ class MultipleSelect extends MultipleOptions
     public function getField($form_name): ?string
     {
         return sprintf(
-            '<select name="%1$s" id="%2$s_%1$s" multiple="multiple">%3$s</select>',
+            '<select name="%1$s[]" id="%2$s_%1$s" multiple="multiple">%3$s</select>',
             $this->name,
             $form_name,
             $this->_buildOptions($this->options['choices'], $this->value)
