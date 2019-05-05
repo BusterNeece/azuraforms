6c0e66f6a458fcfb9ae56952f47c5d1b558a5c31
Buster Neece <loobalightdark@gmail.com>
2019-05-05 12:44:29 -0500

    Add attributeString function to Select.

--- src/Field/Select.php
+++ src/Field/Select.php
@@ -6,7 +6,7 @@ final class Select extends Options
     public function getField($form_name): ?string
     {
         list($attribute_string, $class) = $this->_attributeString();
-        
+
         return sprintf(
             '<select name="%1$s" id="%2$s_%1$s" class="%4%s" %5$s>%3$s</select>',
             $this->getFullName(),
@@ -15,5 +15,25 @@ final class Select extends Options
             $class,
             $attribute_string
         );
+    }
+
+    protected function _attributeString()
+    {
+        $class = '';
+
+        if (!empty($this->error)) {
+            $class = 'error';
+        }
+
+        $attribute_string = '';
+        foreach ($this->attributes as $attribute => $val) {
+            if ($attribute == 'class') {
+                $class .= ' ' . $val;
+            } else if ($val !== false) {
+                $attribute_string .= ' '.($val === true ? $attribute : "$attribute=\"$val\"");
+            }
+        }
+
+        return [$attribute_string, $class];
     }
 }
