6c6315e9d533c9158b3eb455d6a802ab3e7b5ee2
Buster Neece <loobalightdark@gmail.com>
2019-05-05 12:41:23 -0500

    Fix phpstan issues with Field/Select

--- src/Field/Select.php
+++ src/Field/Select.php
@@ -5,8 +5,10 @@ final class Select extends Options
 {
     public function getField($form_name): ?string
     {
+        list($attribute_string, $class) = $this->_attributeString();
+        
         return sprintf(
-            '<select name="%1$s" id="%2$s_%1$s">%3$s</select>',
+            '<select name="%1$s" id="%2$s_%1$s" class="%4%s" %5$s>%3$s</select>',
             $this->getFullName(),
             $form_name,
             $this->_buildOptions($this->options['choices'], $this->value),
