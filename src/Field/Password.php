7072ce11b907fdd547efcf8fe70356964086ed9d
Buster Neece <loobalightdark@gmail.com>
2019-06-27 03:22:26 -0500

    Add password confirmation validation.

--- src/Field/Password.php
+++ src/Field/Password.php
@@ -12,10 +12,22 @@ class Password extends Text
         $this->options['min_length'] = $this->attributes['min_length'] ?? 0;
         unset($this->attributes['min_length']);
 
-        $this->validators[] = function($value) {
+        $this->options['confirm'] = $this->attributes['confirm'] ?? null;
+        unset($this->attributes('confirm'));
+
+        $this->validators[] = function($value, $element) {
             if ($this->options['min_length'] && strlen($value) < $this->options['min_length']) {
                 return sprintf('Must be more than %s characters.', $this->options['min_length']);
             }
+
+            if (!empty($this->options['confirm'])) {
+                $confirm_value = $element->getForm()->getField($this->options['confirm'])->getValue();
+
+                if ($value !== $confirm_value) {
+                    return 'Field and confirmation field do not match.';
+                }
+            }
+
             return true;
         };
     }
